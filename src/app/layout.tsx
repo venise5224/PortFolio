a08a6481973eade16acf871d16ecb2b7b9c1ff2c
JSM <JSM@>
2025-09-08 19:36:44 +0900

    Initial commit: Set up Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "JSM Portfolio",
+  description: "Frontend Developer JSM Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main>
+      <div>HomePage</div>
+    </main>
+  );
+};
+
+export default HomePage;

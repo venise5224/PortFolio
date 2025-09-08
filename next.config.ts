99832c3ba941c923306417a4b77f0e681e87a204
JSM <JSM@>
2025-09-09 01:16:52 +0900

    chore: 다국어 초기 세팅 제거

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import { NextConfig } from "next";
-import nextI18next from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: nextI18next.i18n,
 };
 
 export default nextConfig;
